1b3acdd2c156cb23a08be46ee79326bfbb425439
twugo <hm17031@student.miyazaki-u.ac.jp>
2020-05-17 04:55:07 +0900

    Add: ホームページを追加

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#myAge').text('年齢：' + getAge(1998, 6, 24) + '歳');
+    $(document).bind('touchmove', function(e) {e.preventDefault();}, {passive: false});
+})
+// document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
+/**
+ * calculate age.
+ * @param {number} year - birth year
+ * @param {number} month - birth month
+ * @param {number} day - birth day
+ * @returns {number} age
+ */
+function getAge(year, month, day){
+  var birthday  = new Date(year, month-1, day);
+  var today = new Date();
+  var thisYearBirthday =
+      new Date(today.getFullYear(), month-1, day);  
+
+  var age = today.getFullYear() - birthday.getFullYear();
+  if(today < thisYearBirthday) age -= 1;
+ 
+  return age;
+}
